520050f0fc23d70b6f13f537b9529c6f6d1ea7db
BAUBYTE <paredbaez.martin@gmail.com>
2020-07-22 20:19:14 -0300

    Clase 1

---
+++ Clase_1_HolaMundo/src/clase_1_holamundo/Clase_1_HolaMundo.java
@@ -0,0 +1,60 @@
+package clase_1_holamundo;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Clase_1_HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        /*Para el atajo para el  System.out.println usamos sout y tab*/
+        System.out.println("Otro Hola Mundo");
+
+        /**
+         * Variables y Tipos
+         */
+        int num;
+        String nombrePerro;
+        double estatura;
+        char letra;
+        long numLargo;
+        boolean valorVoF;
+
+        /**
+         * Asignamos Valores
+         */
+        num = 35;
+
+        /**
+         * Suma de Valores
+         */
+        /**
+         * Variables a Usar
+         */
+        int num1, num2, resultado;
+        /**
+         * Asignamos los Valores
+         */
+        num1 = 4;
+        num2 = 3;
+        /**
+         * Calculo los resultados
+         */
+        resultado = num1 + num2;
+        /**
+         * Mostramos el resultado
+         */
+        /**
+         * System.out.println("El Primer Numero que se Ingreso es: " + num1);
+         * System.out.println("El Segundo Numero que se Ingreso es: " + num2);
+         * System.out.println("El Resultado de la Suma es: " + resultado);
+         */
+
+        System.out.println("El Primer Numero que se Ingreso es: " + num1 + " El Segundo Numero que se Ingreso es: " + num2 + " El Resultado de la Suma es: " + resultado);
+    }
+
+}

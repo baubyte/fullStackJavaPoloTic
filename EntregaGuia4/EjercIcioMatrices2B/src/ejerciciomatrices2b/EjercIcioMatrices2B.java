a9f8c115c57014e98a19d3721e6bb86bd54025ab
BAUBYTE <paredbaez.martin@gmail.com>
2020-08-31 18:17:57 -0300

    Edit Consigna

--- EntregaGuia4/EjercIcioMatrices2B/src/ejerciciomatrices2b/EjercIcioMatrices2B.java
+++ EntregaGuia4/EjercIcioMatrices2B/src/ejerciciomatrices2b/EjercIcioMatrices2B.java
@@ -1,4 +1,5 @@
-/** "Consigna del Ejercicio" Llevar a cabo un programa que permita cargar mediante teclado una matriz de 3 x 3
+/** "Consigna del Ejercicio" 
+ * Llevar a cabo un programa que permita cargar mediante teclado una matriz de 3 x 3
  * (3 filas, 3 columnas) con razas de perros. Una vez cargada la misma, será necesario realizar un recorrido por la misma
  * y mostrar los datos cargados por pantalla.
  */
